cb424b56fae086eb0a5885d7186462fe2391e4bd
Tristan Sloughter <t@crashfast.com>
2019-08-25 12:43:31 -0600

    check ct suite's data dir for docker-compose.yml first

--- README.md
+++ README.md
@@ -3,6 +3,14 @@ Common Test Docker Compose Hook
 
 A [Common Test](http://erlang.org/doc/apps/common_test/) hook for starting and stopping [docker-compose](https://docs.docker.com/compose/) services.
 
+To use this hook simply place a `docker-compose.yml` file in the [suite's `data_dir`](http://erlang.org/doc/apps/common_test/write_test_chapter.html#data-and-private-directories), the top level of the project or declare the path explicitly with `docker_compose_file` -- see the configuration section below for more on how the compose file is found.
+
+Then hooks must be declared in the `ct_opts`, the config returned by a function in the test suite or in a [test spec](http://erlang.org/doc/apps/common_test/run_test_chapter.html#test_specifications). Some examples of enabling the hook are in the next section.
+
+Shutdown of the services happens in the hook's `terminate` function which is called depending on when the hook was installed: [CTH Scope](http://erlang.org/doc/apps/common_test/ct_hooks_chapter.html#cth-scope). But the configuration for the hook can include `#{stop => never}` and the hook will not shutdown the services during `terminate`.
+
+## Examples
+
 Hooks can be declared in `rebar.config` with `ct_opts`:
 
 ``` erlang
@@ -30,16 +38,15 @@ init_per_group(Group, Config) ->
     [{ct_hooks, [docker_compose_cth]} | Config].
 ```
 
-Shutdown of the services happens in the hooks `terminate` function which is called depending on when the hook was installed: [CTH Scope](http://erlang.org/doc/apps/common_test/ct_hooks_chapter.html#cth-scope). But the configuration for the hook can include `#{stop => never}` and the hook will not shutdown the services during `terminate`.
-
 ## Configuration
 
+* `docker_compose_file`: The path to a `docker-compose.yml` file to use. If it isn't an absolute path then it is relative to what will be the current working directory during the test run which is `_build/test/logs/ct_run.<node>.<timestamp>`. If no path is given then the hook first checks the suite's `data_dir`, see `test/post_group_SUITE_data/docker-compose.yml` as an example. If no compose file is found in the suite's `data_dir` it will pass no `-f ...` argument to `docker-compose`, this results in `docker-compose` searching up the directory tree (starting at the cwd which in this case is that `ct_run.<node>.<timestamp>` directory mentioned above) until it finds a compile file.
 * `executable_search_paths`: Path to search for the `docker-compose` executable. If it is not set the regular path is searched as described in the [docs for os:find_executable/1](http://erlang.org/doc/man/os.html#find_executable-1).
 * `check_if_running`: A string that is the name of the service to check for before running `docker-compose up`. If defined the hook will check if a service is already up and not call `docker-compose up` if it is. By default this is done by attempting `exec ls` in the service's container, but the command can be set to anything by passing a tuple `{Service, Command}`
 * `skip_or_fail`: The atom `skip` or `fail` (default is `fail`). If the hook is unable to bring up the service with docker-compose then the suite will either skip or fail, depending on this configuration.
 * `stop`: Boolean value that if `false` means the services with not be shutdown when the hook terminates.
 
-## Todo
 
-* Support a path to the `docker-compose.yml` file.
+
+
 
--- src/docker_compose_cth.erl
+++ src/docker_compose_cth.erl
@@ -6,8 +6,11 @@
          post_init_per_group/5,
          terminate/1]).
 
+-include_lib("common_test/include/ct.hrl").
+
 -define(DEFAULTS, #{stop => true,
                     post_init => false,
+                    docker_compose_file => undefined,
                     docker_compose_path => undefined,
                     executable_search_path => undefined,
                     check_if_running => false,
@@ -15,8 +18,9 @@
 
 -type state() :: #{stop => boolean(),
                    post_init => boolean(),
-                   docker_compose_path := filename:filename_all() | undefined,
-                   executable_search_path := [filename:filename_all()],
+                   docker_compose_file => file:filename_all(),
+                   docker_compose_path := file:filename_all() | undefined,
+                   executable_search_path := [file:filename_all()],
                    check_if_running := string() | {string(), [string()]} | false,
                    skip_or_fail := skip | fail}.
 
@@ -50,7 +54,8 @@ do_init(Config, State=#{executable_search_path := Path}) ->
         false ->
             {{fail, "docker-compose not found in PATH"}, State#{post_init => true}};
         DockerCompose ->
-            State1 = State#{docker_compose_path => DockerCompose},
+            State1 = State#{docker_compose_path => DockerCompose,
+                            docker_compose_file => docker_compose_file(Config, State)},
             case up_if_not_running(State1) of
                 ok ->
                     {Config, State1#{post_init => true}};
@@ -59,41 +64,54 @@ do_init(Config, State=#{executable_search_path := Path}) ->
             end
     end.
 
+docker_compose_file(Config, #{docker_compose_file := undefined}) ->
+    DataDir = ?config(data_dir, Config),
+    DataDirCompose = filename:join(DataDir, "docker-compose.yml"),
+    case filelib:is_file(DataDirCompose) of
+        true ->
+            DataDirCompose;
+        false ->
+            undefined
+    end;
+docker_compose_file(_Config, #{docker_compose_file := DockerComposeFile}) ->
+    DockerComposeFile.
+
 terminate(#{stop := false}) ->
     ok;
 terminate(#{docker_compose_path := undefined}) ->
     ok;
-terminate(#{docker_compose_path := DockerCompose}) ->
-    down(DockerCompose).
+terminate(#{docker_compose_path := DockerCompose,
+            docker_compose_file := DockerComposeFile}) ->
+    down(DockerCompose, DockerComposeFile).
 
 %%
 
 -spec up_if_not_running(state()) -> ok | {fail, term()}.
 up_if_not_running(#{docker_compose_path := DockerCompose,
-                    check_if_running := CheckIfRunning}) ->
-    case check_if_running(DockerCompose, CheckIfRunning) of
+                    check_if_running := CheckIfRunning,
+                    docker_compose_file := DockerComposeFile}) ->
+    case check_if_running(DockerCompose, DockerComposeFile, CheckIfRunning) of
         true ->
             ok;
         false ->
-            up(DockerCompose)
+            up(DockerCompose, DockerComposeFile)
     end.
-
 
 find_executable(Executable, undefined) ->
     os:find_executable(Executable);
 find_executable(Executable, Path) ->
     os:find_executable(Executable, Path).
 
-check_if_running(_DockerCompose, false) ->
+check_if_running(_DockerCompose, _DockerComposeFile, false) ->
     false;
-check_if_running(DockerCompose, {Service, Command}) ->
-    0 =:= do(DockerCompose, ["exec", "-T", Service | Command]);
-check_if_running(DockerCompose, Service) ->
-    check_if_running(DockerCompose, {Service, ["ls"]}).
+check_if_running(DockerCompose, DockerComposeFile, {Service, Command}) ->
+    0 =:= do(DockerCompose, DockerComposeFile, ["exec", "-T", Service | Command]);
+check_if_running(DockerCompose, DockerComposeFile, Service) ->
+    check_if_running(DockerCompose, DockerComposeFile, {Service, ["ls"]}).
 
 
-up(DockerCompose) ->
-    case do(DockerCompose, ["up", "-d"]) of
+up(DockerCompose, DockerComposeFile) ->
+    case do(DockerCompose, DockerComposeFile, ["up", "-d"]) of
         127 ->
             {fail, "docker-compose executable not found to run"};
         0 ->
@@ -102,14 +120,19 @@ up(DockerCompose) ->
             {fail, io_lib:format("docker-compose failed to run, exit_status=~p", [E])}
     end.
 
-down(DockerCompose) ->
-    case do(DockerCompose, ["down"]) of
+down(DockerCompose, DockerComposeFile) ->
+    case do(DockerCompose, DockerComposeFile, ["down"]) of
         0 ->
             ok;
         E ->
             ct:pal("Failed to run 'docker-compose down' exit_status=~p", [E]),
             ok
     end.
+
+do(Executable, undefined, Args) ->
+    do(Executable, Args);
+do(Executable, DockerCompileFile, Args) ->
+    do(Executable, ["-f", DockerCompileFile | Args]).
 
 do(Executable, Args) ->
     Port = open_port({spawn_executable, Executable}, [exit_status,
